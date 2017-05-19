ff252f77fed704b66459001bc900a2107c467a30
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-05-19 10:57:47 +0200

    site-header markup ended,about begin

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,14 +1,14 @@
-const gulp=require('gulp');
-const styles =require('gulp-sass');
-const SassModuleImporter= require('sass-module-importer');
 
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sassModuleImporter = require('sass-module-importer');
 
-gulp.task('styles',function(){
+gulp.task('styles', function() {
     return gulp.src('./src/sass/**/*.scss')
-    .pipe(styles({importer:SassModuleImporter()}))
-    .on('error',function(errorInfo){
-        console.log(errorInfo.toString)
-        this.emit('end');
-    })
-    .pipe(gulp.dest('./dist/'));
+        .pipe(sass({ importer: sassModuleImporter() }))
+        .on('error', function(errorInfo) {
+            console.log(errorInfo.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./dist/'));
 })
