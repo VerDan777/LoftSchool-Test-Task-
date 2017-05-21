30b8e057fcfd9494028e4256ef9aaa2a65d95028
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-05-21 17:47:33 +0200

    blocks of gallery fucntionality maked

---
+++ dist/gallery-block.js
@@ -0,0 +1,9 @@
+// import $ from "jquery";
+
+ $('.course__input').change(function(){
+    // setTimeout(color,1000);
+     console.log('yes');
+    $('.course__gallery-block').css('background-color','#ecf4e2');
+    $('.course__gallery-block').remove();
+   
+})
